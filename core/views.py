fa21f730b7cfae50dad78222e8d0c39bdd3ca989
palakknows <20215114.palak@gmail.com>
2024-06-04 22:23:31 +0530

    next commit

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
-from .models import Profile,Post
+from .models import Profile,Post,LikePost
 from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='signin')
@@ -37,7 +37,26 @@ def upload(request):
 
 @login_required(login_url='signin')
 def like_post(request): 
-    pass  
+    username=request.user.username
+    post_id=request.GET.get('post_id')
+
+    post = Post.objects.get(id=post_id)
+
+    like_filter = LikePost.objects.filter(post_id=post_id, username=username).first()
+
+    if like_filter == None:
+        new_like = LikePost.objects.create(post_id=post_id, username=username)
+        new_like.save()
+
+        post.no_of_likes += 1
+        post.save()
+        return redirect('index')
+    else:
+        like_filter.delete()
+
+        post.no_of_likes -= 1
+        post.save()
+        return redirect('index')
 
 @login_required(login_url='signin')
 def settings(request):

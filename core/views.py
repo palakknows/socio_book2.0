3f6a4e407e82485b7b294670471813e94fb0f9f7
palakknows <20215114.palak@gmail.com>
2024-06-07 13:08:32 +0530

    next one

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
-from .models import Profile,Post,LikePost
+from .models import Profile,Post,LikePost,FollowersCount
 from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='signin')
@@ -57,6 +57,26 @@ def like_post(request):
         post.no_of_likes -= 1
         post.save()
         return redirect('index')
+
+@login_required(login_url='signin')
+def follow(request):
+
+    if request.method == 'POST':
+        follower=request.POST['follower']
+        user=request.POST['user']
+
+        if FollowersCount.objects.filter(follower=follower, user=user).first():
+            delete_follower=FollowersCount.objects.get(follower=follower, user=user)
+            delete_follower.delete()
+            return redirect('/profile/'+ user)
+        else:
+            new_follow=FollowersCount.objects.create(follower=follower, user=user)
+            new_follow.save()
+            return redirect('/profile/'+ user)
+    else:
+        return redirect('/')
+    
+    pass
 
 @login_required(login_url='signin')
 def profile(request,pk):

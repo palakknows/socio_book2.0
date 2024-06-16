12ca9120660360ed96e4159697ec82f12f606344
palakknows <20215114.palak@gmail.com>
2024-06-16 11:09:41 +0530

    newyay

--- socio_book/settings.py
+++ socio_book/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'channels',
 ]
 
 MIDDLEWARE = [
@@ -70,6 +71,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'socio_book.wsgi.application'
+
 
 
 # Database
